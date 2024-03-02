b9584046b7903e245fdb500c254fb4f5d65c6f65
DCajiao <dcajiaolazt@gmail.com>
2024-03-02 14:34:29 -0500

    question 1.2 added

--- api/api.py
+++ api/api.py
@@ -66,8 +66,8 @@ def obtener_grafico():
 
     return send_file(img_buffer, mimetype='image/png')
 
-@app.route('/question1', methods=['GET'])
-def statistical_analysis():
+@app.route('/question1.1', methods=['GET'])
+def handed_analysis():
     handed_counts = df['Handed'].value_counts()
     handed_counts_df = handed_counts.to_frame().reset_index()
     handed_counts_df.columns = ['Laterality', 'Quantity']
@@ -87,6 +87,35 @@ def statistical_analysis():
     plt.close()
 
     return send_file(img_buffer, mimetype='image/png')
+
+@app.route('/question1.2', methods=['GET'])
+def handed_analysis_byregion():
+
+    foot_length_by_region = df.groupby(['Region', 'Longer_foot']).size().unstack(fill_value=0)
+
+    plt.figure(figsize=(10, 6))
+    ax = foot_length_by_region.plot(kind='bar', stacked=True)
+
+    plt.title('Number of students by type of foot length and region:')
+    plt.xlabel('Region')
+    plt.ylabel('Number of students')
+    plt.xticks(rotation=45, ha='right')
+    plt.legend(title='Foot Length')
+    plt.tight_layout()
+
+    for p in ax.patches:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+        ax.annotate(f'{height}', (x + width / 2, y + height / 2), ha='center', va='center')
+
+
+    img_buffer = io.BytesIO()
+    plt.savefig(img_buffer, format='png')
+    img_buffer.seek(0)
+    plt.close()
+
+    return send_file(img_buffer, mimetype='image/png') 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
